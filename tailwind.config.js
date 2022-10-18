a8fd4a430bc7779556a53255636aaba01b03bf03
iamzilfa <zilcyam@gmail.com>
2022-10-18 16:02:04 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,15 @@ module.exports = {
         "blackLight" : "#3c4043",
         "grayMobile": "#70757a",
         "grayApp": "#5f6368",
-        "grayButton" : "#dadce0"
+        "grayButton" : "#dadce0",
+        "grayIcon" : "rgba(60,64,67,.08)",
+        "grayShadow" : "0 1px 1px rgb(0 0 0 / 10%)"
+      },
+      boxShadow:{
+        searchshadow:"0 1px 6px rgb(32 33 36 / 28%)",
+        buttonShadow:"0 1px 1px rgb(0 0 0 / 10%);"
       }
+      
     },
   },
   plugins: [],
